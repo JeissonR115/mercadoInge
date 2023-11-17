9c2783d4a30e752ca90c8b743e8959263e0ac53c
jeissonr115 <jeissonfeliper@gmail.com>
2023-11-16 22:21:42 -0500

    envio de formulario

--- server/database/db.php
+++ server/database/db.php
@@ -1,10 +1,16 @@
 <?php
-return function ($dbConfig){
-    $host=$dbConfig['host'];
-    $user=$dbConfig['user'];
-    $password=$dbConfig['password'];
-    $database=$dbConfig['database'];
-    $connection = mysqli_connect($host,$user,$password,$database);
+return function ($dbConfig) {
+    $host = $dbConfig['host'];
+    $user = $dbConfig['user'];
+    $password = $dbConfig['password'];
+    $database = $dbConfig['database'];
+    $connection = new mysqli($host, $user, $password, $database);
+
+    // Verificar la conexión
+    if ($connection->connect_error) {
+        die("Error de conexión: " . $connection->connect_error);
+    }
+
     return $connection;
-}
+};
 ?>
