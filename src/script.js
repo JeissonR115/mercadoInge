8e83cf5648a46aae940b912a87bc32deed8a8252
jeissonr115 <jeissonfeliper@gmail.com>
2023-11-23 02:53:53 -0500

    correcion error menu

--- src/script.js
+++ src/script.js
@@ -3,10 +3,10 @@ import { createCardList} from './card.js'
 const response = await fetch("https://raw.githubusercontent.com/JeissonR115/mercadoInge/main/info.json");
 const data = await response.json();
 
-const menuListElement = document.querySelector(".menu__list");
 const menuIconElement = document.querySelector(".menu__icon");
-
+const menuListElement = document.querySelector(".menu__list");
 menuIconElement.addEventListener("click",()=>{hideAndShowMenu({clickElement:menuIconElement,affectedElement:menuListElement})} );
+menuListElement.addEventListener("click",()=>{hideAndShowMenu({clickElement:menuIconElement,affectedElement:menuListElement})} );
 // createMenuList({parent:menuListElement, listItems: data["menu__list"]})
 
 const summaryListElement = document.querySelector(".team__list");
