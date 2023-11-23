f3a9cbf7ff6f659f2c44ae5e78d8b843a67384db
jeissonr115 <jeissonfeliper@gmail.com>
2023-11-23 00:24:23 -0500

    add singUp

---
+++ server/singUp.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'libs/user.php';
+include_once 'libs/userSession.php';
+
+$user = new User();
+$userSession = new UserSession();
+// Verificar si hay un usuario en sesión
+if (isset($_SESSION['user'])) {
+    $user->setUser($userSession->getCurrentUser());
+    header("Location: /pages/about-us.html");
+} elseif (isset($_POST['emailOrUser']) && isset($_POST['password'])) {
+    $emailOrUserForm = $_POST['emailOrUser'];
+    $passwordForm = $_POST['password'];
+    if ($user->userExist($emailOrUserForm, $passwordForm)) {
+        $userSession->setCurrentUser($emailOrUserForm);
+        $user->setUser($emailOrUserForm);
+        header("Location: /pages/about-us.html");
+    } else {
+        header("Location: /pages/singUp.html?error=1");
+    }
+} else {
+    header("Location: /pages/singUp.htm");
+    exit(); 
+}
