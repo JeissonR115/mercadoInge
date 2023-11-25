8a288e28a789457a73c0675031db13d7458dddaf
jeissonr115 <jeissonfeliper@gmail.com>
2023-11-25 00:18:00 -0500

    cambiar direccion de login

--- server/login.php
+++ server/login.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['user'])) {
     if ($user->userLogin($emailOrUserForm, $passwordForm)) {
         $userSession->setCurrentUser($emailOrUserForm);
         $user->setUser($emailOrUserForm);
-        header("Location: /pages/admin.html");
+        header("Location: /pages/products.js");
     } else {
         header("Location: /pages/login.html?error=1");
     }
